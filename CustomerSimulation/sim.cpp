a7e0c823006fce151dbe06ec3fd5850cd37fa65c
dabbydunkerton <wyakhli@ilstu.edu>
2017-09-25 11:57:27 -0500

    More minor changes(commenting and small errors"

--- CustomerSimulation/sim.cpp
+++ CustomerSimulation/sim.cpp
@@ -33,33 +33,33 @@ int main(){
 	//setting up first customer to arrive
 	pa = rand()%x+1;
 	//this for loop simulates a 12 hour shift where each i increment is 1 minute
-	for(int i=1;i<720;i++){
+	for(int i=1; i<720; i++){
 		//if customer arrives
 		if(pa=i){
 			cout<<"Customer "<<customer<<" arrived at "<<i<<endl;
 			customer++;
 			//check if line que and being currently served is empty
-			if(size=0 && bcs=0){
+			if(size = 0 && bcs = 0){
 				bcs = (rand()%x+1) + i;
 				pa = (rand()%x+1) + i;
 			}
 			else{
 				//someone else is already being serviced,wait in line,Then schdule next customer
-				line.add(rand()%x+1);
+				line.add(rand()%x + 1);
 				//add the current time so when customer finishes waiting in line you can compare the times and get wait time
 				time.add(i);
 				size++;
-				pa = (rand()%x+1) + i;	
+				pa = (rand()%x + 1) + i;	
 			}
 		
 		
 		}
 		//if current customer is done being serviced
-		if(bcs=i){
+		if(bcs = i){
 			cout<<"Customer "<<customerL<<" left at "<<i<<endl;
 			customerL++;
 			//this if statment checks to make sure that there is a customer waiting in line
-			if(size>0){
+			if(size > 0){
 				bcs = line.remove+i;
 				size--;
 				wait = i-time.remove;
@@ -67,16 +67,16 @@ int main(){
 		
 		}
 		//checks the size of the line once a minute
-		if(size>maxSize){
+		if(size > maxSize){
 			maxSize = size;
 		}
 		//checks the wait time against the max
-		if(wait>maxWait){
+		if(wait > maxWait){
 			maxWait = wait;
 		}
 		//reseting these values
-		wait=0;
-		size=0;
+		wait = 0;
+		size = 0;
 	}
 	cout<<"The longest the line was at any minute during the simulation was "<<maxSize<<" Customers."<<endl;
 	cout<<"The longest an indivudal customer waited in line was "<<maxWait<<" minutes"<<endl; 
