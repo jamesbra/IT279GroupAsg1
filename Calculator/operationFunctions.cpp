1b57744edc459acb8793eb78c2a89ea050291bd1
Brandon James <brandonjames879@gmail.com>
2017-09-24 21:54:05 -0500

    Fixed undo/redo problem - still has full undo problem

--- Calculator/operationFunctions.cpp
+++ Calculator/operationFunctions.cpp
@@ -6,45 +6,45 @@
 void operationFunctions::addition(int& output,calcNode& temp,ourStack<calcNode>& undoStack,ourStack<calcNode>& redoStack)
 {
   temp.operandOne = output;
-  undoStack.push(temp);
   output+=temp.operandTwo;
   temp.result=output;
+  undoStack.push(temp);
   redoStack.clear();
 }
 
 void operationFunctions::subtraction(int& output,  calcNode& temp,ourStack<calcNode>& undoStack,ourStack<calcNode>& redoStack)
 {  
   temp.operandOne = output;
-  undoStack.push(temp);
   output-=temp.operandTwo;
   temp.result=output;
+  undoStack.push(temp);
   redoStack.clear();
 }
 
 void operationFunctions::multiplication(int& output,  calcNode& temp,ourStack<calcNode>& undoStack,ourStack<calcNode>& redoStack)
 {
   temp.operandOne = output;
-  undoStack.push(temp);
   output=output*temp.operandTwo;
   temp.result=output;
+  undoStack.push(temp);
   redoStack.clear();
 }
 
 void operationFunctions::division(int& output,  calcNode& temp,ourStack<calcNode>& undoStack,ourStack<calcNode>& redoStack)
 {
   temp.operandOne = output;
-  undoStack.push(temp);
   output=output/temp.operandTwo;
   temp.result=output;
+  undoStack.push(temp);
   redoStack.clear();
 }
 
 void operationFunctions::modulo(int& output,  calcNode& temp,ourStack<calcNode>& undoStack,ourStack<calcNode>& redoStack)
 {
   temp.operandOne = output;
-  undoStack.push(temp);
   output=output%temp.operandTwo;
   temp.result=output;
+  undoStack.push(temp);
   redoStack.clear();
 }
 
@@ -77,8 +77,8 @@ void operationFunctions::redo(int& output,  calcNode& temp,ourStack<calcNode>& u
 void operationFunctions::clear(int& output,  calcNode& temp,ourStack<calcNode>& undoStack,ourStack<calcNode>& redoStack)
 {
   temp.operandOne = output;
-  undoStack.push(temp);
   output=0;
   temp.result=output;
+  undoStack.push(temp);
   redoStack.clear();
 }
